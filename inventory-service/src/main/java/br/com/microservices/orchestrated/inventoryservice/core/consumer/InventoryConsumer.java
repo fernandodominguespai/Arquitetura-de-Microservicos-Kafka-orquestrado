ea4867fa2f828574a05076a6ff24b98a63e48aef
Fernando Domingues dos Santos <fsantos.ekan@iamspe.sp.gov.br>
2025-04-11 17:37:09 -0300

    48 - Criando a logica de rollback no estoque

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class InventoryConsumer {
 
-//    private final InventoryService inventoryService;
+    private final InventoryService inventoryService;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
@@ -21,8 +22,8 @@ public class InventoryConsumer {
     public void consumeSuccessEvent(String payload) {
         log.info("Receiving success event {} from inventory-success topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
-//        inventoryService.updateInventory(event);
+//        log.info(event.toString());
+        inventoryService.updateInventory(event);
     }
 
     @KafkaListener(
@@ -32,7 +33,7 @@ public class InventoryConsumer {
     public void consumeFailEvent(String payload) {
         log.info("Receiving rollback event {} from inventory-fail topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
-//        inventoryService.rollbackInventory(event);
+//        log.info(event.toString());
+        inventoryService.rollbackInventory(event);
     }
 }
